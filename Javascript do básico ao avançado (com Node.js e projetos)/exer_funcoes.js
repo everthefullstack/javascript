45ada1f578358a5a36562dc115958c7f47516777
everthefullstack <giovani.lima@everthefullstack.com.br>
2020-07-26 21:34:36 -0300

    Atualização dos arquivos do curso

--- Javascript do básico ao avançado (com Node.js e projetos)/exer_funcoes.js
+++ Javascript do básico ao avançado (com Node.js e projetos)/exer_funcoes.js
@@ -96,3 +96,274 @@ function decrementaImprimePar(num){
     }
 }
 decrementaImprimePar(10);
+
+//criar array com 5 itens, acessar o indice 1,3 e 4
+let arrayCriado1 = [0,1,2,3,4];
+console.log(arrayCriado1[1]);
+console.log(arrayCriado1[3]);
+console.log(arrayCriado1[4]);
+
+//criar dois arrays e imprimir o numero de elementos de cada um
+arrayCriado1 = [0,1];
+let arrayCriado2 = [0,1,2,3];
+console.log(arrayCriado1.length);
+console.log(arrayCriado2.length);
+
+//crie um objeto onibus com 8 rodas, 40 passageiros, 2 portas e imprima as propriedades
+let objOnibus ={
+    rodas: 8,
+    passageiros: 40,
+    portas: 2
+};
+console.log(objOnibus.rodas);
+console.log(objOnibus.passageiros);
+console.log(objOnibus.portas);
+
+//adicionar a propriedade janela e deletar a propriedade rodas
+objOnibus.janelas = 20;
+delete objOnibus.rodas;
+console.log(objOnibus);
+
+//criar um array com 5 nomes e verificar se existe determinado nome
+let nomes = ["Bruna", "Marcelo", "Giovani", "Hero", "Roney"];
+if(nomes.includes("Pedro")){
+    console.log("Existe esse nome");
+} else {
+    console.log("Não existe o nome procurado");
+}
+
+//cria dois arrays, um com 5 elementos e outro com menos
+//fazer função que verifica o numero de elementos
+//se tiver menos de 5, imprima poucos elementos
+//se tiver mais imprima muitos elementos
+let num1 = [1,2,3,4,5];
+let num2 = [1,2,3,4];
+let num3 = [1,2,3,4,5,6]
+
+function verificaElementos(arrays){
+    if(arrays.length > 5){
+        console.log("muitos elementos");
+    } else if(arrays.length < 5){
+        console.log("poucos elementos");
+    } else {
+        console.log("tem 5 elementos");
+    }
+}
+verificaElementos(num1);
+verificaElementos(num2);
+verificaElementos(num3);
+
+//criar array com 5 elementos e interar entre todos eles
+for(let i = 0; i < num1.length; i++){
+    console.log(num1[i]);
+};
+
+let jsonObj = {
+    "nome": "Giovani",
+    "idade": "29",
+    "profissao": "Desenvolvedor"
+};
+console.log(jsonObj.nome);
+console.log(jsonObj.idade);
+console.log(jsonObj.profissao);
+
+//criar um array a parti de uma frase
+let frase = "Eu sou um desenvolvedor muito bom";
+let vetorFrase = frase.split(" ");
+for(let i = 0; i < vetorFrase.length; i++){
+    console.log(vetorFrase[i]);
+};
+
+//criar um objeto calculadora com as 4 operações básicas
+const calculadora = {    
+    soma: function(n1, n2){
+        return n1 + n2;
+    },
+
+    sub: function(n1, n2){
+        return n1 - n2;
+    },
+
+    mult: function(n1, n2){
+        return n1 * n2;
+    },
+
+    div: function(n1, n2){
+        if(n2 === 0){
+            console.log("Não pode dividir por zero");
+        } else{
+            return n1 / n2;
+        }
+    }
+}
+console.log(calculadora.soma(1,2));
+console.log(calculadora.sub(1,2));
+console.log(calculadora.mult(1,2));
+console.log(calculadora.div(1,2));
+
+//criar uma classe simulando um banco com propriedade saldo, metodo saque e depósito
+class Conta{
+    constructor(saldo){
+        this.saldo = saldo
+    }
+    deposito(saldo){
+        this.saldo = this.saldo + saldo;
+    }
+    saque(saldo){
+        this.saldo = this.saldo - saldo;
+    }
+}
+
+//criar uma classe que simula um carrinho de compras
+//propriedade itens, quantidade e valor
+//criar metodo para adicionar e remover itens
+class Item{
+    constructor(id,item, qtd, valor){
+        this.id = id;
+        this.item = item;
+        this.qtd = qtd;
+        this.valor = valor;
+    }
+}
+class Carrinho{
+
+    lista = [];
+
+    add(item){
+        this.lista.push(item);
+    }
+
+    rmv(item){
+        for(let itemCarrinho in this.lista){
+            if(this.lista[itemCarrinho].id == item.id){
+                this.lista.splice(this.lista.indexOf(this.lista[itemCarrinho]), 1);
+                console.log("Removido");
+            }
+        }
+    }
+}
+let carrinho = new Carrinho();
+let item1 = new Item(1,"Calça", 1, 50);
+let item2 = new Item(2,"Cueca", 3, 90);
+carrinho.add(item1);
+carrinho.add(item2);
+carrinho.rmv(item1);
+carrinho.rmv(item2);
+for(let itemCarrinho in carrinho.lista){
+    console.log(carrinho.lista[itemCarrinho]);
+}
+
+//crie um objeto que simula um endereço de um cliente
+//propriedades: rua, bairro, cidade e estado
+//no construtor, o endereço ja deve ser definido por completo
+//criar metodos para atualizar todas as propriedades
+class Endereco{
+    constructor(rua, bairro, cidade, estado){
+        this.rua = rua;
+        this.bairro = bairro;
+        this.cidade = cidade;
+        this.estado = estado
+    }
+    set setRua(rua){
+        this.rua = rua;
+    }
+    set setBairro(bairro){
+        this.bairro = bairro;
+    }
+    set setCidade(cidade){
+        this.cidade = cidade;
+    }
+    set setEstado(estado){
+        this.estado = estado;
+    }
+}
+let endereco = new Endereco("Rua são nicolau 850", "Estancia velha", "Canoas", "RS");
+endereco.setEstado = "Rio Grande do Sul";
+console.log(endereco);
+
+//criar uma classe carro
+//propriedades marca, cor, gasolina restante
+class Carro{
+    constructor(marca, modelo, tanque){
+        this.marca = marca;
+        this.modelo = modelo;
+        this.tanque = tanque;
+    }
+    dirigir(){
+
+        if(this.tanque == 0){
+            console.log("Carro está sem gasolina, abasteça.");
+        } else {
+            while(this.tanque > 0){
+                console.log("Combustivel restante = " + this.tanque);
+                this.tanque --;
+            }
+        }
+    }
+    abastecer(gasolina){
+        this.tanque = this.tanque + gasolina;
+    }
+}
+let carro = new Carro("Ford", "Focus", 50);
+console.log(carro);
+carro.abastecer(50);
+console.log(carro);
+carro.dirigir();
+carro.dirigir();
+
+//criar uma classe conta bancaria
+//pripriedades saldo na conta corrente, saldo na polpança e juros da polpança
+//metodos depósito e saque e transferir dinheiro da poupança pra conta corrente e vice versa
+//criar conta especial herdando da conta normal que dobra os juros da polpança
+class ContaNormal{
+    constructor(saldoConta, saldoPolp, jurosPolp){
+        this.saldoConta = saldoConta;
+        this.saldoPolp = saldoPolp;
+        this.jurosPolp = jurosPolp;
+    }
+    depositoCC(valor){
+        this.saldoConta = this.saldoConta + valor;
+    }
+    depositoP(valor){
+        this.saldoPolp = this.saldoPolp + valor;
+    }
+    transfCc(valor){
+        this.saldoPolp = this.saldoPolp - valor;
+        this.saldoConta = this.saldoConta + valor;
+    }
+    transfP(valor){
+        this.saldoConta = this.saldoConta - valor;
+        this.saldoPolp = this.saldoPolp + valor;
+    }
+    saqueCc(valor){
+        this.saldoConta = this.saldoConta - valor;
+    }
+    saqueP(valor){
+        this.saldoPolp = this.saldoPolp - valor;
+    }
+    jurosBonus(){
+        this.saldoPolp = this.saldoPolp * 0.2 + this.saldoPolp
+    }
+}
+let conta = new ContaNormal(1000,100,1);
+class ContaEspecial extends ContaNormal{
+    constructor(saldoConta, saldoPolp, jurosPolp){
+        super(saldoConta,saldoPolp,jurosPolp*2)
+    }
+}
+let contaEspecial = new ContaEspecial(100,200,1);
+conta.saqueCc(800);
+conta.saqueP(500);
+conta.depositoCC(800);
+conta.depositoP(300);
+conta.transfCc(100);
+conta.transfP(800);
+conta.jurosBonus();
+contaEspecial.saqueCc(100);
+contaEspecial.saqueP(200);
+contaEspecial.depositoCC(800);
+contaEspecial.depositoP(300);
+contaEspecial.transfCc(100);
+contaEspecial.transfP(800);
+contaEspecial.jurosBonus();
+console.log(contaEspecial);
--- Javascript do básico ao avançado (com Node.js e projetos)/script.js
+++ Javascript do básico ao avançado (com Node.js e projetos)/script.js
@@ -212,3 +212,286 @@ let jsonToString = JSON.stringify(json);
 console.log(jsonToString);
 let stringToJson = JSON.parse(jsonToString);
 console.log(stringToJson)
+
+//orientação a objetos - metodos
+const cachorro1 = {
+    uivar: function()  {
+        console.log("Auuuuuuuuuu");
+    },
+    rosnar: function()  {
+        console.log("Grrrrrrrr");
+    }
+}
+cachorro1.uivar();
+cachorro1.rosnar();
+
+//getter e setter manualmente
+const cachorro2 = {
+    raca: "SRD",
+    uivar: function()  {
+        console.log("Auuuuuuuuuu");
+    },
+    rosnar: function()  {
+        console.log("Grrrrrrrr");
+    },
+    setRaca: function(raca) {
+        this.raca = raca;
+    },
+    getRaca: function(){
+        return this.raca;
+    }
+}
+console.log(cachorro2.raca);
+cachorro2.uivar();
+cachorro2.rosnar();
+cachorro2.setRaca("Pitbull");
+console.log(cachorro2.getRaca());
+
+//prototypes
+const cachorro3 = {
+    patas: 2
+}
+const cachorro4 = Object.create(cachorro3);
+console.log(cachorro4.patas);
+
+//classes
+const cachorro5 = {
+    patas:4,
+    raca: "SRD",
+    latir: function(){
+        console.log("Au au");
+    }
+}
+let labrador = Object.create(cachorro5);
+
+//construtores de classe
+function criaCachorro(raca, patas, cor){
+    let cachorro6 = Object.create({});
+    cachorro6.raca = raca;
+    cachorro6.patas = patas;
+    cachorro6.cor = cor;
+    return cachorro6;
+}
+let doberman = criaCachorro("Doberman", 4, "preto");
+console.log(doberman);
+
+//instanciando classes por new
+function Cachorro1(raca, patas, cor){
+    this.raca = raca;
+    this.patas = patas;
+    this.cor = cor;
+    this.latir = function(){
+        console.log("Au au")
+    }
+}
+let cachorro7 = new Cachorro1("husky", 4, "branco");
+console.log(cachorro7);
+
+//metodos e prototypes
+function Cachorro2(raca, patas, cor){
+    this.raca = raca;
+    this.patas = patas;
+    this.cor = cor;
+}
+Cachorro2.prototype.latir = function() {
+        console.log("Au au");
+}
+
+//construtor de classes no ES6
+class Cachorro3 {
+    constructor(raca, patas, cor){
+        this.raca = raca;
+        this.patas = patas;
+        this.cor = cor;
+    }
+}
+let cachorro8 = new Cachorro3("Labrador", 4, "Preto");
+console.log(cachorro8);
+
+//override no prototype
+Cachorro3.prototype.raca = "SRD";
+console.log(Cachorro3.prototype.raca);
+console.log(cachorro8.raca);
+
+//metodos e propriedades de uma class ES6
+class Cachorro4 {
+    constructor(raca, patas, cor){
+        this.raca = raca;
+        this.patas = patas;
+        this.cor = cor;
+    }
+    latir(){
+        console.log("Au au");
+    }
+}
+let cachorro9 = new Cachorro4("Jaguara", 4, "Preto");
+cachorro9.latir();
+
+//synbols, propriedades unicas que não podem ser alteradas nem criadas duas vezes
+class Cachorro5 {
+    constructor(raca, cor){
+        this.raca = raca;
+        this.cor = cor;
+    }
+    latir(){
+        console.log("Au au");
+    }
+}
+let patas = Symbol();
+Cachorro5.prototype[patas] = 4;
+console.log(Cachorro5.prototype[patas]);
+
+//getters e setters ES6
+class Cachorro6 {
+    constructor(raca, cor){
+        this.raca = raca;
+        this.cor = cor;
+    }
+    latir(){
+        console.log("Au au");
+    }
+
+    get getCor(){
+        return this.cor;
+    }
+
+    set setCor(cor){
+        this.cor = cor;
+    }
+}
+let cachorro10 = new Cachorro6("SRD", "preto");
+cachorro10.setCor = "Branco";
+console.log(cachorro10.getCor);
+
+//herança
+class Mamifero {
+    constructor(patas){
+        this.patas = patas;
+    }
+}
+class Coiote extends Mamifero{
+    constructor(patas, cor){
+        super(patas, patas);
+        this.cor = cor;
+    }
+    rosnar(){
+        console.log("Grrrrrrrrrrrr");
+    }
+}
+let coiote = new Coiote(4, "Marrom");
+console.log(coiote.patas);
+coiote.rosnar();
+
+//instance of para verificar quem é o pai do objeto
+console.log(new Coiote instanceof Mamifero);
+
+//strict impede do javascript ser php, "use strict"
+
+//debugger serve para colocar um breakpoint para debugar o código
+
+//assertion é uma verificação no programa para assegurar que tudo ocorra
+//da forma esperada
+
+//Expressões regulares são uma forma de encontrar padrões em strings
+//são um tipo de objeto que podem ser instanciados:
+let reg1 = new RegExp("teste");
+let reg2 = /test/;
+console.log(reg1.test("tem a palavra teste aqui?"));
+console.log(reg2.test("Cachorro"));
+
+//Definir um conjunto de caracteres para ser verificado
+console.log(/[0-9]/.test("Existe o numero 7 aqui?"));
+console.log(/[123]/.test("Existe o numero 123 aqui?"));
+
+//caracteres especiais
+// \d qualquer digito de caractere
+// \w um caractere alfanumerico
+// \s qualquer caractere com espaço em branco
+// \D caracteres que não sejam numeros
+// \W caractere não alfanumerico
+// \S caractere que não seja espaço em branco
+// .  qualquer caractere menos nova linha
+const pontoRegex = /./;
+console.log(pontoRegex.test("asd"));
+console.log(pontoRegex.test(" "));
+console.log(pontoRegex.test("123"));
+console.log(pontoRegex.test("123asd"));
+
+const dRegex = /\d/;
+console.log(dRegex.test("asd"));
+console.log(dRegex.test(" "));
+console.log(dRegex.test("123"));
+console.log(dRegex.test("123asd"));
+
+const dRegex2 = /\D/;
+console.log(dRegex2.test("asd"));
+console.log(dRegex2.test(" "));
+console.log(dRegex2.test("123"));
+console.log(dRegex2.test("123asd"));
+
+const sRegex = /\s/;
+console.log(sRegex.test("asd"));
+console.log(sRegex.test(" "));
+console.log(sRegex.test("123"));
+console.log(sRegex.test("123asd"));
+
+const wRegex = /\w/;
+console.log(wRegex.test("asd"));
+console.log(wRegex.test(" "));
+console.log(wRegex.test("123"));
+console.log(wRegex.test("123asd"));
+
+//regex com caracteres especiais
+const dia = /\d\d/
+console.log(dia.test("2019"));
+console.log(dia.test("asd"));
+console.log(dia.test("05"));
+
+//operador not aceitam tudo menos alguns caracteres
+const notab = /[^ab]/;
+console.log(notab.test("abb"));
+console.log(notab.test("aqui tem a"));
+
+//operador plus mostra um elemento se repetindo indefinidamente
+const umOuMais = /\d+/;
+console.log(umOuMais.test("1518"));
+console.log(umOuMais.test(""));
+
+//operador question faz com o que o digito anterior seja opcional
+const padrao = /Abaca?i/;
+console.log(padrao.test("Abacaxi"));
+console.log(padrao.test("Abacai"));
+
+//operador de precisão insere o numero de ocorencias entre {}
+const cep = /\d{5}-\d{3}/
+console.log(cep.test("90250-761"));
+console.log(cep.test("90240370"));
+
+//metodo exec retorna um objeto com algumas informações a mais
+const digitos = /\d+/;
+console.log(digitos.exec("12365"));
+console.log(digitos.exec("teste"));
+
+//metodo match funciona semelhante ao exec
+const frase = "o número 100 está aqui".match(/\d+/);
+console.log(frase);
+
+//choice pattern pode colocar uma instrução na regex como se fosse o "or-||"
+const reg = /\w+: (Giovani|Bruna|Hero)/;
+console.log(reg.test("Nome: Giovani"));
+console.log(reg.test("Nome: Gustavo"))
+
+//Validando um domínio
+const validaDominio = /www?.\w+\.com.br|.com/;
+console.log(validaDominio.test("www.google.com"));
+console.log(validaDominio.test("www.google.com.br"));
+console.log(validaDominio.test("google.com"));
+
+//validar email
+const email = /\w+@\w+\.\w+/;
+console.log(email.test("giovani.lima@everthefullstack.com.br"));
+
+//validar data de nascimento
+const dataNasc = /[0-9]{2}[/][0-9]{2}[/][0-9]{4}/
+console.log(dataNasc.test("07/07/1991"));
+console.log(dataNasc.test("07/07/91"));
